93428f1dedde7d5fc12062dd89d9b9304c39f144
KSanWapiti <sananikone.kevin@gmail.com>
2020-03-30 13:01:00 +0200

    Lvl Up !
    Lvl Effect

--- RPG/Assets/Scripts/PlayerController/PlayerStats.cs
+++ RPG/Assets/Scripts/PlayerController/PlayerStats.cs
@@ -7,6 +7,7 @@ public class PlayerStats : MonoBehaviour
     public int currentXp;
     public int currentLvl;
     public int[] xpArray;
+    public GameObject lvlEffect;
 
     // Start is called before the first frame update
     void Start()
@@ -20,11 +21,14 @@ public class PlayerStats : MonoBehaviour
         if(currentXp >= xpArray[currentLvl])
         {
             currentLvl += 1;
+            Instantiate(lvlEffect, transform.position, transform.rotation);
+
         }
     }
 
     public void addXp(int xpToAdd)
     {
         currentXp += xpToAdd;
+        
     }
 }
