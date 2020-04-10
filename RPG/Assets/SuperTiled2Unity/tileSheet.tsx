c5a224813d4cb03d3d8630b9eb8115735a1ac927
KSanWapiti <sananikone.kevin@gmail.com>
2020-04-10 19:04:30 +0200

    Dangerous commit in coming

---
+++ RPG/Assets/SuperTiled2Unity/tileSheet.tsx
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.3" name="tileSheet" tilewidth="16" tileheight="16" spacing="1" tilecount="22261" columns="113">
+ <image source="../Art/SpriteSheets/tileSheet.png" width="1920" height="3360"/>
+ <tile id="5515">
+  <objectgroup draworder="index">
+   <object id="1" x="8" y="7"/>
+  </objectgroup>
+ </tile>
+</tileset>
