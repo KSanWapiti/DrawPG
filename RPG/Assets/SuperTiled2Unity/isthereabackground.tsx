508d3c29ee4d8551e6c17bcb3e6f0e71dc37061c
Ironem <yang_jia@telecom-sudparis.eu>
2020-05-16 22:40:27 +0200

    shield ajouté et connexion
    connexions des differents mondes
    Je le met la pour ne pas perdre ce que j'ai fais et pouvoir recup les maps de kevin

---
+++ RPG/Assets/SuperTiled2Unity/isthereabackground.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.4" name="isthereabackground" tilewidth="15" tileheight="15" spacing="1" tilecount="21000" columns="140">
+ <image source="../Prefabs/Sprite Sheet/Map/isthereabackground.png" width="2240" height="2400"/>
+ <tile id="2960" probability="0.1"/>
+ <tile id="5203" probability="0.1"/>
+ <tile id="5204" probability="0.1"/>
+</tileset>
