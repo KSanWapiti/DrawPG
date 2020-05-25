91255b4daaa607c270482e8bcacc9edae7ed15ee
Antoine1906 <Antoine1906@users.noreply.github.com>
2020-05-25 18:23:30 +0200

    Les Quêtes

---
+++ RPG/Assets/Scripts/Quete 2/Script_talisman_png.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script_talisman_png : MonoBehaviour
+{
+    public bool showGUI=false;
+    public GameObject talisman;
+    public bool is_talisman;
+    void Start()
+    {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            showGUI = true;
+        }
+
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            showGUI = false;
+        }
+
+    }
+
+    void OnGUI()
+    {
+        if (showGUI)
+        {
+            if (is_talisman == false)
+            {
+                GUI.BeginGroup(new Rect(Screen.width / 2 - 150, Screen.height / 2 + 15, 300, 145));
+                GUI.Box(new Rect(0, 0, 300, 145), "Bijoutière étourdie");
+                GUI.Label(new Rect(20, 30, 250, 120), "(Cherche son talisman) Impossible de retrouver ce satané talisman ... Peut-être l'aurais-je égarer lors de ma balade dans la forêt ?");
+                if (GUI.Button(new Rect(60, 95, 180, 40), "Lui promettre que vous allez" + "\n" + "jeter un oeil"))
+                {
+                    talisman.SetActive(true);
+                    GameObject.FindObjectOfType<Script_talisman_png>().enabled = false;
+
+                }
+            }
+            if(is_talisman==true)
+            {
+                GUI.BeginGroup(new Rect(Screen.width / 2 - 150, Screen.height / 2 + 15, 300, 145));
+                GUI.Box(new Rect(0, 0, 300, 100), "Bijoutière étourdie");
+                GUI.Label(new Rect(20, 30, 250, 90), "Mon précieux talisman! Tu l'as retrouvé!! Merci beaucoup, je ferai plus attention à l'avenir.");
+
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ RPG/Assets/Scripts/Quete 3/Script_enigmes_png.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script_enigmes_png : MonoBehaviour
+{
+    public bool showGUI;
+    public GameObject player;
+    bool isAnswered = false;
+    void Start()
+    {
+        
+    }
+
+    void OnGUI()
+    {
+        if (showGUI)
+        {
+            if (isAnswered==false)
+            {
+                GUI.BeginGroup(new Rect(Screen.width / 2 - 150, Screen.height / 2 + 15, 300, 185));
+                GUI.Box(new Rect(0, 0, 300, 185), "Vieillard énigmatique");
+                GUI.Label(new Rect(20, 30, 250, 120), "Combien y-a-il de planètes dans le système solaire ?  ");
+                if (GUI.Button(new Rect(60, 70, 180, 30), "9"))
+                {
+                    player.transform.position = new Vector3(-152, 76, 0);
+
+                }
+                if (GUI.Button(new Rect(60, 105, 180, 30), "8"))
+                {
+                    isAnswered = true;
+                }
+                if (GUI.Button(new Rect(60, 140, 180, 30), "10")) {
+                    player.transform.position = new Vector3(-154, 86, 0);
+                }
+            }
+            if(isAnswered)
+			{
+                GUI.BeginGroup(new Rect(Screen.width / 2 - 150, Screen.height / 2 + 15, 300, 100));
+                GUI.Box(new Rect(0, 0, 300, 185), "Vieillard énigmatique");
+                GUI.Label(new Rect(20, 30, 250, 120), "Bravo! Tu sembles être bien parti pour devenir mon digne héritier ! ");
+
+            }
+        
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            showGUI = true;
+        }
+
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            showGUI = false;
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
